c45bfbfc4c3753f2289e6f344b48f9572195c328
[nboyaroca] <[nboyaroca@gmail.com]>
2022-07-08 08:34:56 +0200

    more data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,3 +22,51 @@ INTO
   (comment, moment_id)
 VALUES
   ('comment1', 1);
+INSERT
+INTO
+  moments
+  (title, description, img_url)
+VALUES
+  ('Tokyo', 'Japan''s capital', 'https://i.pinimg.com/originals/18/90/51/1890510f3aedb106fb0da13172403cc6.jpg');
+INSERT
+INTO
+  moments
+  (title, description, img_url)
+VALUES
+  ('Sydney', 'Important city in Australia', 'https://i.pinimg.com/474x/20/ae/cc/20aecc98f3829c2e27e0f04c96457158.jpg');
+INSERT
+INTO
+  moments
+  (title, description, img_url)
+VALUES
+  ('París', 'France capital', 'https://www.kevinandamanda.com/wp-content/uploads/2015/11/best-neighborhoods-to-explore-paris-france-walking-tours-01-480x720.jpg');
+INSERT
+INTO
+  moments
+  (title, description, img_url)
+VALUES
+  ('Moscow', 'Russia''s capital', 'http://www.robertharding.com/watermark.php?type=preview&im=RM/RH/VERTICAL/817-421385');
+INSERT
+INTO
+  moments
+  (title, description, img_url)
+VALUES
+  ('Athens', 'Greece''s capital', 'https://thumbs.dreamstime.com/b/acr%C3%B3polis-de-parten%C3%B3n-atenas-grecia-athens-greece-disparo-vertical-182186976.jpg');
+INSERT
+INTO
+  comments
+  (comment, moment_id)
+VALUES
+  ('City of LOVE', 6);
+INSERT
+INTO
+  comments
+  (comment, moment_id)
+VALUES
+  ('Athens is considered Democracy''s birthplace ', 8);
+INSERT
+INTO
+  comments
+  (comment, moment_id)
+VALUES
+  ('The gods chose the best site for their Olympus', 8);
