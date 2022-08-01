459d06471463e6296937fe69a7785e8364e852a8
[nboyaroca] <[nboyaroca@gmail.com]>
2022-08-01 12:37:58 +0200

    Fix NotFoundException update & delete

--- src/main/java/com/factoria/moments/services/momentS/MomentService.java
+++ src/main/java/com/factoria/moments/services/momentS/MomentService.java
@@ -54,7 +54,7 @@ public class MomentService implements IMomentService{
     public Moment updateMoment(Long id, MomentRequestDto updatedMoment, User authUser) {
         var moment = momentRepository.findById(id);
         if (moment.isEmpty()) throw new NotFoundException("Moment doesn't exist", "M-404");
-        if (moment.get().getPublisher()==authUser) throw new BadRequestException("Only the publisher can update his moment", "M-007");
+        if (moment.get().getPublisher()!=authUser) throw new BadRequestException("Only the publisher can update his moment", "M-007");
         moment.get().setTitle(updatedMoment.getTitle());
         moment.get().setDescription(updatedMoment.getDescription());
         moment.get().setImgUrl(updatedMoment.getImgUrl());
@@ -64,9 +64,10 @@ public class MomentService implements IMomentService{
 
     @Override
     public boolean deleteById(Long id, User authUser) {
-        var moment= momentRepository.findById(id).get();
-        if (moment.getPublisher()==authUser) throw new BadRequestException("Only the publisher can delete his moment", "M-008");
-        this.momentRepository.delete(moment);
+        System.out.println(authUser);
+        var moment= momentRepository.findById(id);
+        if (moment.get().getPublisher()!=authUser) throw new BadRequestException("Only the publisher can delete his moment", "M-008");
+        this.momentRepository.delete(moment.get());
         return true;
     }
 
